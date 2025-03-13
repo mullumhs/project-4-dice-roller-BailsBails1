37b32bf909b5ce261254677c1449c9928c3c39e3
BailsBails1 <147569909+BailsBails1@users.noreply.github.com>
2025-03-13 23:40:06 +0000

    nearly finished lab 3

--- lab3.py
+++ lab3.py
@@ -10,17 +10,27 @@
 # Define a function called get_number(prompt) that returns an integer. 
 # Include a while loop and try/except blocks inside the function to handle non-integer inputs.
 
-def get_number(prompt):
-    
-    while True:
-      
-      num=input("please enter a")
-      
+
+  #makes a function to get a number
+
+
+def get_number():
+      #makes a while true loop to continue the program forever if an invalid input is entered
+      while True:
+        #asks user for a number and stores it 
         try:
-             
-            num=input(int(prompt))
+          number=int(input("enter a number: "))
+          return(number)
+         #if user enters anything but a valid number a prompt is displayed and question is asked again 
         except:
-            print("please enter a number")
+          print("please enter a valid integer")
+          
+  #makes the function under x and prints the answer.
+
+
+
+   
+    
             
 
 
@@ -34,6 +44,24 @@ def get_number(prompt):
 # Divide the numerator by the denominator and print the result, handling any division by zero errors.
 
 
+while True:
+  try:
+      numerator= get_number()
+      denominator=get_number()
+      
+      answer=(numerator/denominator)
+      print(answer)
+      break
+     
+      
+      
+  except:
+      print("please enter a valid number")
+
+     
 
 # Use the get_number function to ask the user for an index to access an element from a predefined list. 
 # Print out the value from the list, handling the index error if the user selects a non-existent index.
+
+my_list = ["beer","wine","spirits"]
+
