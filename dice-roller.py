51f387165e5e768e2d0027a86da00f29ce9de5be
BailsBails1 <147569909+BailsBails1@users.noreply.github.com>
2025-03-17 22:49:19 +0000

    started dice roller

--- dice-roller.py
+++ dice-roller.py
@@ -6,3 +6,52 @@
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#show each individual roll
+#pick type of die
+#pick how many rolls
+#input validation
+
+
+
+#title.
+print("welcome to dice roller!")
+
+
+
+
+#import random
+import random
+
+
+
+#make function that takes number of sides 
+
+def get_sides():
+
+    while True:
+        try:
+            sides=input(int(prompt))
+            
+            return sides 
+        
+        except:
+            print("please enter a valid choice")
+
+
+
+
+get_sides()
+
+
+'''
+def output():
+    for i in range(qty)
+
+'''
+
+
+#print output with total.
+
+
+
+
